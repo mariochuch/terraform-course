4144dc93906b13c65f10a6dfa1f8f91f541fe909
Mariusz Chucherko <mariusz.chucherko@mcsystems.com.pl>
2024-12-09 08:36:42 +0100

    feat: adding common tags and use them in the TF code

--- _solutions/06-resources-solution/networking.tf
+++ _solutions/06-resources-solution/networking.tf
@@ -1,31 +1,33 @@
+locals {
+  common_tags = {
+    ManagedBy  = "Terraform"
+    Project    = "06-resources-solution"
+    CostCenter = "12345"
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
-  tags = {
-    Name      = "06-resources-solution-main"
-    ManagedBy = "Terraform"
-    Project   = "06-resources-solution"
-  }
+  tags = merge(local.common_tags, {
+    Name = "06-resources-solution"
+  })
 }
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/24"
 
-  tags = {
-    Name      = "06-resources-solution-main"
-    ManagedBy = "Terraform"
-    Project   = "06-resources-solution"
-  }
+  tags = merge(local.common_tags, {
+    Name = "06-resources-solution-public"
+  })
 }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name      = "06-resources-solution-main"
-    ManagedBy = "Terraform"
-    Project   = "06-resources-solution"
-  }
+  tags = merge(local.common_tags, {
+    Name = "06-resources-solution-main"
+  })
 }
 
 resource "aws_route_table" "public" {
@@ -35,11 +37,9 @@ resource "aws_route_table" "public" {
     gateway_id = aws_internet_gateway.main.id
   }
 
-  tags = {
-    Name      = "06-resources-solution-public"
-    ManagedBy = "Terraform"
-    Project   = "06-resources-solution"
-  }
+  tags = merge(local.common_tags, {
+    Name = "06-resources-solution-public"
+  })
 }
 
 resource "aws_route_table_association" "public" {
