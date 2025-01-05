f2941edbb231a413b21c5f4a6017ac74216592dc
Mariusz Chucherko <mariusz.chucherko@mcsystems.com.pl>
2025-01-05 20:29:21 +0100

    new: added new security group and ingress rules for http and https.

--- _solutions/06-resources-solution/compute.tf
+++ _solutions/06-resources-solution/compute.tf
@@ -3,9 +3,41 @@ resource "aws_instance" "web" {
   associate_public_ip_address = true
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.public_http_traffic.id]
   root_block_device {
     delete_on_termination = true
     volume_size           = 10
     volume_type           = "gp3"
   }
+
+  tags = merge(local.common_tags, {
+    Name = "06-resources-solution-ec2"
+  })
+}
+
+resource "aws_security_group" "public_http_traffic" {
+  description = "Allowing public traffic on ports 443 and 80"
+  name        = "public-http-traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(local.common_tags, {
+    Name = "06-resources-solution-sg"
+  })
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
 }
