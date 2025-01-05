09db4a05cc1e8305d9694bb5b73e788676618a1f
Mariusz Chucherko <mariusz.chucherko@mcsystems.com.pl>
2025-01-05 19:45:31 +0100

    new: added ec2 instance (web)

--- _solutions/06-resources-solution/compute.tf
+++ _solutions/06-resources-solution/compute.tf
@@ -1,5 +1,11 @@
 resource "aws_instance" "web" {
-  ami = "ami-0a628e1e89aaedf80"
+  ami                         = "ami-0a628e1e89aaedf80"
   associate_public_ip_address = true
-  instance_type = "t2.micro"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 10
+    volume_type           = "gp3"
+  }
 }
