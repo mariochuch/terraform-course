f21e4262352ba96932013d6a3667ee496c6f8100
Mariusz Chucherko <mariusz.chucherko@mcsystems.com.pl>
2025-01-15 20:20:55 +0100

    new: displaying iam policy document using datasources

--- _solutions/07-data-sources/compute.tf
+++ _solutions/07-data-sources/compute.tf
@@ -27,6 +27,28 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+data "aws_iam_policy_document" "static_website" {
+  statement {
+    sid = "PublicReadGetObject"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = ["s3:GetObject"]
+
+    resources = ["${aws_s3_bucket.public_read_bucket.arn}/*"]
+  }
+}
+
+resource "aws_s3_bucket" "public_read_bucket" {
+  bucket = "my-public-read-bucket"
+}
+
+output "iam_policy" {
+  value = data.aws_iam_policy_document.static_website
+}
+
 output "azs" {
   value = data.aws_availability_zones.available
 }
