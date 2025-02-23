72fdd91e9d7696996f4cf9284ad3ceb6be02a540
Mariusz Chucherko <mariusz.chucherko@mcsystems.com.pl>
2025-02-23 09:29:03 +0100

    new: working with outputs

--- _solutions/08-input-vars-locals-outputs/shared-locals.tf
+++ _solutions/08-input-vars-locals-outputs/shared-locals.tf
@@ -13,3 +13,18 @@ locals {
     managed_by    = local.managed_by
   }
 }
+
+output "s3_project_bucket_name" {
+  value = aws_s3_bucket.project_bucket.bucket
+  description = "The name of the S3 bucket"
+}
+
+output "instance_arn" {
+  value = aws_instance.compute.arn
+  description = "Tags added to volume"
+}
+
+output "s3_bucket_tags" {
+  value = aws_s3_bucket.project_bucket.tags_all
+  description = "List of tags added to s3 bucket"
+}
