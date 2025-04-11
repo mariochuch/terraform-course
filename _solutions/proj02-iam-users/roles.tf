0e841efdf33095208f6c33280d71c173b9d2576d
Mariusz Chucherko <mariusz.chucherko@mcsystems.com.pl>
2025-04-11 13:12:46 +0200

    new: define assumed role policies for each role

--- _solutions/proj02-iam-users/roles.tf
+++ _solutions/proj02-iam-users/roles.tf
@@ -15,6 +15,21 @@ locals {
       "SecurityAudit"
     ]
   }
+
+  role_policies_list = flatten([
+    for role, policies in local.role_policies : [
+      for policy in policies : {
+        role   = role
+        policy = policy
+      }
+    ]
+  ])
+
+}
+
+output "policies" {
+  value = local.role_policies_list
+
 }
 
 data "aws_iam_policy_document" "assumed_role_policy" {
@@ -31,4 +46,20 @@ resource "aws_iam_role" "roles" {
   for_each           = toset(keys(local.role_policies))
   name               = each.key
   assume_role_policy = data.aws_iam_policy_document.assumed_role_policy.json
+}
+
+data "aws_iam_policy" "managed_policies" {
+  for_each = toset(local.role_policies_list[*].policy)
+  arn      = "arn:aws:iam::aws:policy/${each.key}"
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachments" {
+  count = length(local.role_policies_list)
+  role = aws_iam_role.roles[
+    local.role_policies_list[count.index].role
+  ].name
+  policy_arn = data.aws_iam_policy.managed_policies[
+    local.role_policies_list[count.index].policy
+  ].arn
+
 }
