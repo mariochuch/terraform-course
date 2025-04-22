5183600b8b1580ba95017852dca6976a4dd7a037
Mariusz Chucherko <mariusz.chucherko@mcsystems.com.pl>
2025-04-22 08:14:57 +0200

    new: bulding my own VPC module

---
+++ _solutions/13-local-modules/modules/networking/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_cidr" {
+  type = string
+  validation {
+    condition     = can(cidrnetmask(var.vpc_cidr))
+    error_message = "The vpc_cidr must contain a valid CIDR block."
+  }
+}
