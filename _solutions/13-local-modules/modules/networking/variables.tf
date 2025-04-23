0f04d72ba10a8d715d28af4d7499bec7fe297975
Mariusz Chucherko <mariusz.chucherko@mcsystems.com.pl>
2025-04-23 08:25:08 +0200

    new: using object variables

--- _solutions/13-local-modules/modules/networking/variables.tf
+++ _solutions/13-local-modules/modules/networking/variables.tf
@@ -1,7 +1,13 @@
-variable "vpc_cidr" {
-  type = string
+
+variable "vpc_config" {
+  type = object({
+    cidr_block = string
+    name       = string
+  })
+
   validation {
-    condition     = can(cidrnetmask(var.vpc_cidr))
+    condition     = can(cidrnetmask(var.vpc_config.cidr_block))
     error_message = "The vpc_cidr must contain a valid CIDR block."
   }
+  
 }
