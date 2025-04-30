9ee72c9f76ac419c74133f9553fd3b7504bedd9a
Mariusz Chucherko <mariusz.chucherko@mcsystems.com.pl>
2025-04-30 08:42:09 +0200

    new: validation of received availabity zones

--- _solutions/13-local-modules/modules/networking/vpc.tf
+++ _solutions/13-local-modules/modules/networking/vpc.tf
@@ -1,3 +1,7 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "this" {
   cidr_block = var.vpc_config.cidr_block
 
@@ -10,10 +14,19 @@ resource "aws_subnet" "this" {
   for_each = var.subnet_config
 
   vpc_id            = aws_vpc.this.id
-  cidr_block        = each.value.cidr_block
   availability_zone = each.value.az
+  cidr_block        = each.value.cidr_block
 
   tags = {
     Name = each.key
+  }
+
+  lifecycle {
+    precondition {
+      condition     = contains(data.aws_availability_zones.available.names, each.value.az)
+      error_message = <<-EOT 
+      Subnet key: 
+      EOT
+    }
   }
 }
